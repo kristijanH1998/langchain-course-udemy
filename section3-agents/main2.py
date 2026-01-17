0256b662758009119805250f519242bb0d28a90a
kristijanH1998 <kristijan.hornung1@gmail.com>
2026-01-16 16:29:05 -0800

    tavily tool within langchain

--- section3-agents/main2.py
+++ section3-agents/main2.py
@@ -1,3 +1,5 @@
+# run with: uv run main.py
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -5,25 +7,11 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-tavily = TavilyClient()
-
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns: 
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
 
 llm = ChatOpenAI(model="gpt-5")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
