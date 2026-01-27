24fbe00255f2abc16027793c05e671fa3236bb33
kristijanH1998 <kristijan.hornung1@gmail.com>
2026-01-27 02:52:39 -0800

    project setup

---
+++ sec11-langgraph/main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
+
+
+if __name__ == "__main__":
+    print("Hello ReAct LangGraph with Function Calling")
+    # print(os.getenv("OPENAI_API_KEY"))
+
+
+
