6badead8ec8ee19e87979ce94818d7a3156dfccf
kristijanH1998 <kristijan.hornung1@gmail.com>
2026-01-27 21:36:25 -0800

    graph result

--- sec11-langgraph/main.py
+++ sec11-langgraph/main.py
@@ -35,6 +35,6 @@ app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 if __name__ == "__main__":
     print("Hello ReAct LangGraph with Function Calling")
     # print(os.getenv("OPENAI_API_KEY"))
-
-
+    res = app.invoke({"messages": [HumanMessage(content="What is the temperature in Tokyo? List it and then triple it")]})
+    print(res["messages"][LAST].content)
 
