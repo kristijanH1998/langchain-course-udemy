d0c7849221f4a6c6bc88cffae6eaeae02d329433
kristijanH1998 <kristijan.hornung1@gmail.com>
2026-01-29 01:39:29 -0800

    revisor agent

--- sec13-reflexion-agent/schemas.py
+++ sec13-reflexion-agent/schemas.py
@@ -14,5 +14,11 @@ class AnswerQuestion(BaseModel):
         description="1-3 search queries for researching improvements to adress the critique of your current answer."
     )
 
+class ReviseAnswer(AnswerQuestion):
+    """Revise your original answer to your question."""
+    references: List[str] = Field(
+        description="Citations motivating your updated answer."
+    )
+
 
 
