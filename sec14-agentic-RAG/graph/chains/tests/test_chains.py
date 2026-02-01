8083130b546a7668261d7ea1799bade622435c5d
kristijanH1998 <kristijan.hornung1@gmail.com>
2026-01-31 18:41:58 -0800

    query routing to either vectorstore or web search

--- sec14-agentic-RAG/graph/chains/tests/test_chains.py
+++ sec14-agentic-RAG/graph/chains/tests/test_chains.py
@@ -6,7 +6,7 @@ from graph.chains.generation import generation_chain
 from ingestion import retriever
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from graph.chains.hallucination_grader import hallucination_grader, GradeHallucinations
-
+from graph.chains.router import question_router, RouteQuery
 
 def test_retrieval_grader_answer_yes() -> None:
     question = "agent memory"
@@ -51,5 +51,18 @@ def test_hallucination_grader_answer_no() -> None:
         {"documents": docs, "generation": "some rubbish text"}
     )
     assert not res.binary_score
+
+def test_router_to_vectorstore() -> None:
+    question = "agent memory"
+    res: RouteQuery = question_router.invoke({"question": question})
+    assert res.datasource == "vectorstore"
+
+def test_router_to_websearch() -> None:
+    question = "how to make pizza"
+    res: RouteQuery = question_router.invoke({"question": question})
+    assert res.datasource == "websearch"
+
+
+
 
 
